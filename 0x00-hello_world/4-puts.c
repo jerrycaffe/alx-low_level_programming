6b383884790cef756b63208b4276c02f5f46f904
Adeleye Jeremiah <jadeleye@efficion.org>
2023-02-17 13:08:53 +0100

    printing words in C programming language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	char value[] = "Programming is like building a multilingual puzzle";
+	char value[] ="\ "Programming is like building a multilingual puzzle";
 
 	puts(value);
 	return 0;
