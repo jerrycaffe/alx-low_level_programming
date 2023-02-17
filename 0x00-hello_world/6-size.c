9ae761fac76840d53eafc8aad98f4f58fda22335
Adeleye Jeremiah <jadeleye@efficion.org>
2023-02-17 14:16:01 +0100

    size of some data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: %2d byte(s)\n",(unsigned long)sizeof(char));
 	printf("Size of an int: %2d byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %2d byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long: %2d byte(s)\n", (unsigned long)sizeof(long long));
+	printf("Size of a long long int: %2d byte(s)\n", (unsigned long)sizeof(long long int));
 	printf("Size of a float: %2d byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
