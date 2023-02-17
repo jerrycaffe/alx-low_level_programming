ae561f9d61fc042bfd5a57f9f50b0633f6d8e8de
Adeleye Jeremiah <jadeleye@efficion.org>
2023-02-17 16:39:57 +0100

    unix system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	puts("and that piece of art is useful\"");
-	fputs(" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"
+	+" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
