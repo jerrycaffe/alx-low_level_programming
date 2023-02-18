3b92de5c5848e1efb0735023da51b2fa3bc665da
Adeleye Jeremiah <jadeleye@efficion.org>
2023-02-18 04:17:58 +0100

    working on detect zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,26 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Determine if a number is positive, negative or zero.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n >0)
+	if (n > 0)
 	{
-		printf("%d is positive\n", n);	
+		printf("%d is positive\n", n);
 	}
-	if(n == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	if(n < 0)
+	else
 	{
 		printf("%d is negative\n", n);
 	}
