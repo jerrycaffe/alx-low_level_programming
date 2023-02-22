79ac349812aad0afcfaf6ec87b9bd63d2166dc5f
jerrycaffe <jerrycaffe@gmail.com>
2023-02-22 13:58:10 -0800

    return the last number in a given value

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,22 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_last_digit - print last digit
- *       value of an integer
+ * print_last_digit - print last digit of a number.
  *
- * @n: takes in integer type input for function
+ * @n: takes number input
  *
- * Return: Always 0 (Success)
+ * Return: lastDigit
 */
 
 int print_last_digit(int n)
 {
-	int remainder;
-	remainder = n % 10;
-	return (remainder);
+	int lastDigit;
+
+	if (n < 0)
+		lastDigit = -1 * (n % 10);
+	else
+		lastDigit = n % 10;
+
+	_putchar(lastDigit + '0');
+	return (lastDigit);
 }
