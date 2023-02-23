06e1aa26a88e99de37d77af47b6c8a0c4602fdc7
jerrycaffe <jerrycaffe@gmail.com>
2023-02-22 20:14:28 -0800

    fixing error in add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,17 +1,15 @@
 #include"main.h"
 
 /**
- * add  adds two integers
+ * add - adds two integers
  *
  *@a: input value 1
  *
  *@b: input value 2
-
  *
- * 
+ *
 */
 int add(int a, int b)
 {
 	return (a + b);
 }
-
