987421fe4185071e203ff0575f73a98fe8bf6483
jerrycaffe <jerrycaffe@gmail.com>
2023-02-23 14:18:31 -0800

    print numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,11 +10,11 @@
 void print_numbers(void)
 {
 	int num = 0;
-
-	do {
+	while (num >= 0 && num <= 9)
+	{
 		_putchar(num + 48);
 		num++;
-	} while (num >= 0 && num <= 9);
+	}
 	_putchar('\n');
 }
 
