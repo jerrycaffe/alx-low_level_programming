510b1794df323a11e211b93925f2143aa5847001
jerrycaffe <jerrycaffe@gmail.com>
2023-02-23 18:45:53 -0800

    corrected 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,9 @@
 void print_most_numbers(void)
 {
 	int num = 0;
-
-	do {
-		if (num != 2 || num != 4)
-			_putchar(num + 48);
+	while (num <= 9)
+		if (num != 2 && num != 4)
+			_putchar(num + '0');
 		num++;
-	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
