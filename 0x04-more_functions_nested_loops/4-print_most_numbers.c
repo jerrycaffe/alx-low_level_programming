2ab5f2cde51a7d84968336118fce4cb3f5416b59
jerrycaffe <jerrycaffe@gmail.com>
2023-02-23 18:58:58 -0800

    corrected 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,18 +3,20 @@
 #include"main.h"
 
 /**
- * _islower - function to check if
- *           character is lowercase
+ * print_most_numbers - function to check if
+ *           it is 2 or 4 n print
  *
- * @c: checks input of function
  *
- * Return: returns 1 if `c` is lowercase
- *         otherwise always 0 (Success)
+ * Return: returns 0 always
+ *         
 */
-
-int _islower(int c)
+void print_most_numbers(void)
 {
-	if (c >= 97 && c <= 122)
-		return (1);
-	return (0);
+	int num;
+	num = 0;
+	while (num <= 9)
+		if (num != 2 && num != 4)
+			_putchar(48 + num);
+		num++;
+	_putchar('\n');
 }
