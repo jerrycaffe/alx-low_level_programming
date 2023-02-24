a40355aaed24c6dbd97acbe34b4c5b826f8b4756
jerrycaffe <jerrycaffe@gmail.com>
2023-02-23 19:04:40 -0800

    corrected 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * print_most_numbers - function to check if
- *           it is 2 or 4 n print
- *
- *
- * Return: returns 0 always
- *         
+ * print_most_numbers - print 0 - 9 apart
+ *                 from 2 and 4 and you can
+ *                 only use _putchar twice
+ * Return: Always 0 (Success)
 */
+
 void print_most_numbers(void)
 {
 	int num;
-	num = 0;
-	while (num <= 9)
-		if (num != 2 && num != 4)
-			_putchar(48 + num);
-		num++;
+
+	for (num = 0; num <= 9; ++num)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
+	}
 	_putchar('\n');
 }
