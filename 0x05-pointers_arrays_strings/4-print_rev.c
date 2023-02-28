cb9a6f16854002962e4b235be7a0f693a203036a
jerrycaffe <jerrycaffe@gmail.com>
2023-02-28 13:51:44 -0800

    printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - print in reverse
+ *
+ * @s: input value
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; ++count);
+
+	for(--count; count >= 0; --count)
+		_putchar(s[count]);
+	_putchar('\n');
+}
