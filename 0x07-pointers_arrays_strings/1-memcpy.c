d16467eaa540a43c0042333bceb2bbd09f4f47d4
jerrycaffe <jerrycaffe@gmail.com>
2023-03-06 19:20:42 -0800

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @dest: destination
+ * @src: value to copy
+ * @n: n bytes
+ *
+ * Return: Always 0
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+		dest[i] =src[i];
+		i++;
+	return (dest);
+}
