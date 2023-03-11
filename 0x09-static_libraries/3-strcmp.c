59d058c4e1f028f62ddbbf5e6ebf1e6e8ec36159
jerrycaffe <jerrycaffe@gmail.com>
2023-03-11 12:06:09 -0800

    still working on C

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,24 +1,49 @@
-#include "holberton.h"
+
 
 /**
- * _puts - print a string
+ * _strcmp - a function that compares two strings
  *
- * @str: string pointer
+ * @s1: string 1 input to compare
+ * @s2: against this other string 2
  *
- * Return: nothing
+ * Return: 0 if s1 and s2 are equal
+ *        negative integer if the stopping character
+ *                in @s1 was less than the stopping
+ *                character in @s2
+ *        positive integer if the stopping character
+ *                in @s1 was greater than the stopping
+ *                character in @s2
 */
 
-void _puts(char *str)
+int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0, r;
 
-	for (i = 0; str[i] >= 0; i++)
+	/**
+	 * iterate through src and
+	 * compare it with dest
+	*/
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (str[i] == '\0')
+		/**
+		 * if they differ by a single character
+		 * don't iterate further
+		*/
+		if (s1[i] != s2[i])
 		{
-			_putchar('\n');
+			/*
+			 * return the difference between
+			 * the two characters
+			*/
+			r = s1[i] - s2[i];
 			break;
 		}
-		_putchar(str[i]);
+		else
+		{
+			r = s1[i] - s2[i];
+		}
+		i++;
 	}
+
+	return (r);
 }
