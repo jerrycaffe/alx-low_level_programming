3a8e50ae2cff7c0400b446d19da6e0dce0b26d94
jerrycaffe <jerrycaffe@gmail.com>
2023-03-24 19:54:50 -0700

    added the sum of them all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,27 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-
 #include <stdarg.h>
-#include <stdio.h>
-
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 
 /**
- * struct print - A new struct type defining our print
+ * sum_them_all - adds all its parameters
  *
- * @symbol: Our symbol representing the data type
- * @print_func: a function pointer that prints
- *             a data type according to the symbol
+ * @n: start of input variables
+ *
+ * Return: the sum
 */
-typedef struct print
-{
-	char *symbol;
-	void (*print_func)(va_list arg);
-} func_printer;
 
-#endif /* VARIADIC_FUNCTIONS_H */;
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int param, sum = 0;
+
+	/* initialize the argument list from the start */
+	va_start(ap, n);
+
+	/* iterate through all parameter values*/
+	for (param = 0; param < n; param++)
+		/* get the next parameter value and add it to sum*/
+		sum += va_arg(ap, int);
+	/*Clean up*/
+	va_end(ap);
+
+	return (sum);
+}
