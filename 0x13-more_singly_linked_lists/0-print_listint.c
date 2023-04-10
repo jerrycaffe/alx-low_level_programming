b99681ca314d3664cf3e368aca74eabd1b7912b7
jerrycaffe <jerrycaffe@gmail.com>
2023-04-10 06:15:47 -0700

    this is printing of linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all in linked list
+ * @h: pointer to head
+ * Return: the number of nodes
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 1;
+
+	if(h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+
+		h = h->next;
+
+		count++;
+
+	}
+
+	printf("%d\n", h->n);
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly list define
+ * @n: expected int
+ * @next: pointer to the next node
+ *
+ * Description: structure for node linked list
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif /* LISTS_H */
