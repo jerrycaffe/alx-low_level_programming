370f099cac5e707a2444e9a48a89ed4c3b5463ae
jerrycaffe <jerrycaffe@gmail.com>
2023-04-10 07:37:32 -0700

    corrected value for adding another element

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,32 +1,30 @@
 #include "lists.h"
 
 /**
- * add_nodeint - add node at the start
+ * add_nodeint - will add new node
  *
  * @head: pointer to the first node
+ * @n: value to number
  *
- * @n: value in the node
- *
- * Returns: addres to new node or NULL
- *
- */
+ * Return: address to newly added node
+*/
 
 listint_t *add_nodeint(listint_t **head, const int n)
-{       
+{
 	listint_t *node;
-	
+
 	node = malloc(sizeof(listint_t));
-     	if (node == NULL)
+	if (node == NULL)
 		return (NULL);
-     
+
 	if (*head == NULL)
 		node->next = NULL;
 	else
 		node->next = *head;
-	
+
 	node->n = n;
-	
+
 	*head = node;
-	
+
 	return (*head);
-}       
+}
