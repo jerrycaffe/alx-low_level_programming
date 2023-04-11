4ad480e23014880196a770fe714d8e31fd39f3bc
jerrycaffe <jerrycaffe@gmail.com>
2023-04-10 21:34:02 -0700

    reading file from a file

---
+++ 0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - read text printout in POSIX
+ *
+ * @filename: named file to be read
+ * @letters: letters to read
+ *
+ * Return: 0 incase of failure or file
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int new_file;
+	ssize_t check, count;
+	char *buffer;
+
+	if (filename == NULL) 
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (new_file == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	check = read(file, buffer, letters);
+	if (check == -1) 
+		return (0);
+
+	count = write(STDOUT_FILENO, buffer, check);
+	if (count == -1 || check != count)
+		return (0);
+
+	free(buffer);
+
+	close(file);
+
+	return (count);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is about file input and output in C
