b35fbdea9bc5b4e28ed44796725a00911b59339d
jerrycaffe <jerrycaffe@gmail.com>
2023-04-10 21:48:07 -0700

    correcting

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,36 +10,28 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int new_file;
-	ssize_t check, count;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-
-	if (new_file == -1)
-		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
 
-	check = read(file, buffer, letters);
-	if (check == -1)
-		return (0);
-
-	count = write(STDOUT_FILENO, buffer, check);
-	if (count == -1 || check != count)
-		return (0);
-
 	free(buffer);
+	close(o);
 
-	close(file);
-
-	return (count);
+	return (w);
 }
